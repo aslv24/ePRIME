b88afa24bfd8801ee96dd32b07889dc94ab533f5
aslv24 <aslinfomats@gmail.com>
2025-08-29 17:44:17 +0530

    Assertion added

--- ePRIME/src/test/java/com/ePRIME/TestScripts/SampleRun.java
+++ ePRIME/src/test/java/com/ePRIME/TestScripts/SampleRun.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import com.ePRIME.PageObjects.LoginPage;
 import com.ePRIME.TestBase.BaseClass;
@@ -15,7 +16,13 @@ import com.ePRIME.TestUtility.ListenersUtility;
 @Listeners(ListenersUtility.class)
 public class SampleRun extends BaseClass{
 	
+	String expectedTitle="HMS";
+	
+	String expectedUrl="http://localhost/hmsci";
+	
 	LoginPage loginPage;
+	
+	SoftAssert softAssert;
 	
 	@Parameters({"browserName"})
 	@BeforeTest
@@ -32,15 +39,23 @@ public class SampleRun extends BaseClass{
 	@Test(priority = 1)
 	public void verifyTitle()
 	{
+		softAssert=new SoftAssert();
 		String pageTitle = DriverUtility.pageTitle();
+		softAssert.assertEquals(pageTitle, expectedTitle);
+		System.out.println("This line will execute only if assertion passes.");
 		System.out.println(pageTitle);
+		softAssert.assertAll();
 	}
 	
 	@Test(priority = 2)
 	public void verifyUrl()
 	{
+		softAssert=new SoftAssert();
 		String pageUrl = DriverUtility.pageUrl();
+		softAssert.assertEquals(pageUrl, expectedUrl);
+		System.out.println("This line will execute only if assertion passes.");
 		System.out.println(pageUrl);
+		softAssert.assertAll();
 	}
 	
 	
